e0440f9a652445b76ca7bf4d050702aeb45a8219
Wern Ancheta <ancheta.wern@gmail.com>
2018-10-10 22:14:44 +0800

    add code for animating switching of Pokemon

--- src/components/PokemonFullSprite/PokemonFullSprite.js
+++ src/components/PokemonFullSprite/PokemonFullSprite.js
@@ -1,11 +1,45 @@
 import React, { Component } from "react";
-import { Image, Animated } from "react-native";
+import { View, Image, Animated, Easing } from "react-native";
 
 class PokemonFullSprite extends Component {
   constructor(props) {
     super(props);
+    this.pokeball_y_translate = new Animated.Value(0);
+    this.pokeball_opacity = new Animated.Value(0);
+
     this.sprite_translateY = new Animated.Value(0);
+    this.sprite_scale = new Animated.Value(0);
   }
+
+  componentDidMount() {
+    this.animateSwitchPokemon();
+  }
+
+  animateSwitchPokemon = () => {
+    this.sprite_translateY.setValue(0);
+    this.pokeball_opacity.setValue(0);
+    this.pokeball_y_translate.setValue(0);
+    this.sprite_scale.setValue(0);
+
+    Animated.sequence([
+      Animated.timing(this.pokeball_y_translate, {
+        toValue: 1,
+        easing: Easing.bounce,
+        duration: 1000
+      }),
+
+      Animated.timing(this.pokeball_opacity, {
+        toValue: 1,
+        duration: 200,
+        easing: Easing.linear
+      }),
+
+      Animated.timing(this.sprite_scale, {
+        toValue: 1,
+        duration: 500
+      })
+    ]).start();
+  };
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.isAlive !== this.props.isAlive && !this.props.isAlive) {
@@ -13,8 +47,8 @@ class PokemonFullSprite extends Component {
         duration: 900,
         toValue: 1
       }).start();
-    } else if (prevProps.isAlive !== this.props.isAlive && this.props.isAlive) {
-      this.sprite_translateY.setValue(0);
+    } else if (prevProps.pokemon !== this.props.pokemon && this.props.isAlive) {
+      this.animateSwitchPokemon();
     }
   }
 
@@ -28,21 +62,54 @@ class PokemonFullSprite extends Component {
       outputRange: [0, 1000]
     });
 
+    const pokemon_scale = this.sprite_scale.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [0, 0.5, 1]
+    });
+
+    const pokeball_moveY = this.pokeball_y_translate.interpolate({
+      inputRange: [0, 1, 2],
+      outputRange: [0, 50, 25]
+    });
+
+    const pokeball_opacity = this.pokeball_opacity.interpolate({
+      inputRange: [0, 0.5, 1],
+      outputRange: [1, 0.5, 0]
+    });
+
     return (
-      <Animated.Image
-        source={sprite}
-        resizeMode={"contain"}
-        style={[
-          styles.image,
-          {
+      <View>
+        <Animated.Image
+          source={require("../../assets/images/things/pokeball.png")}
+          style={{
             transform: [
               {
-                translateY: pokemon_moveY
+                translateY: pokeball_moveY
               }
-            ]
-          }
-        ]}
-      />
+            ],
+
+            opacity: pokeball_opacity
+          }}
+        />
+
+        <Animated.Image
+          source={sprite}
+          resizeMode={"contain"}
+          style={[
+            styles.image,
+            {
+              transform: [
+                {
+                  translateY: pokemon_moveY
+                },
+                {
+                  scale: pokemon_scale
+                }
+              ]
+            }
+          ]}
+        />
+      </View>
     );
   }
 }
